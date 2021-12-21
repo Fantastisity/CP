d7c75dba7ce833f948929e2fa3ede82c6c77f710
Jack <61994603+Fantastisity@users.noreply.github.com>
2021-12-21 16:48:26 +1100

    Add files via upload

---
+++ algebra/binary exponentiation/spojZSUM.cpp
@@ -0,0 +1,89 @@
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+const int dir[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+
+#define mp make_pair
+#define nl puts("")
+#define pb push_back
+#define fst(v) (*v.cbegin())
+#define lst(v) (*v.crbegin())
+#define tint(s, i) (s[i] - '0')
+#define cint(i) (i + '0')
+#define tchar(s, i) (s[i] - 'a')
+#define FA(i, v) for (auto& i : (v))
+#define F(i, a, b) for (int i = a; i < b; ++i)
+#define FE(i, a, b) for (int i = a; i <= b; ++i)
+#define FG(i, a, b) for (int i = a; i > b; --i)
+#define FGE(i, a, b) for (int i = a; i >= b; --i)
+#define tr(it, x, ed) for (auto it = (x).begin(), ed = (x).end(); it != ed; ++it)
+#define eq(a, b) (!(a ^ b))
+#define sz(x) ((int)((x).size()))
+#define lbit(x) ((x & (-x))) // get lowest bit
+#define all(c) (c).begin(),(c).end()
+#define rall(c) (c).rbegin(),(c).rend()
+#define mid(l, r) (l + ((r - l) >> 1))
+#define lft(l) (l << 1)
+#define rht(r) ((r << 1) + 1)
+#define has(v, ele) (v.find(ele) != v.end())
+template<typename... args>
+void dbg(args... arg) {
+	__attribute__((unused)) auto _ = {((void)(cout << arg << "  "), 0)...};
+    nl;
+}
+#define chq printf("%s\n", "current line is ok...")
+#define scan(d) scanf("%d", &d)
+#define scan2(a, b) scanf("%d%d\n", &a, &b)
+#define scans(s) scanf("%s", s)
+#define initd(d) memset(d, 0, sizeof(d))
+#define loc(v, ele) lower_bound(v.cbegin(), v.cend(), ele) - v.cbegin()
+
+const int MAXN = 10000;
+const int MOD = 10000007;
+
+typedef vector<int> vi;
+using ll = long long;
+using ul = unsigned long;
+using ull = unsigned long long;
+
+struct pair_comp {
+    inline bool operator() (const pair<int, int>& a, const pair<int, int>& b) const {
+        return a.first < b.first && a.second < b.second;
+    }
+};
+
+ull binp(ull x, ull y) {
+	x %= MOD;
+	ull res = 1;
+	while (y) {
+		if (y & 1) res = res * x % MOD;
+		x = x * x % MOD;
+		y >>= 1;
+	}
+	return res;
+}
+
+int n, k;
+
+void solve() {
+	ull sn = 0, sn1 = 0, sn2 = 0, pn = 0, pn1 = 0, pn2 = 0;
+	FE(i, 1, n) {
+		sn += binp(i, k);
+		pn += binp(i, i);
+		if (i == n - 1) sn1 = sn, pn1 = pn;
+		if (i == n - 2) sn2 = sn, pn2 = pn;
+	}
+	sn += pn, sn1 += pn1, sn2 += pn2;
+	dbg((sn + sn1 - (sn2 << 1)) % MOD);
+}
+
+int main() {
+    freopen("/home/drac/Documents/CP/input.txt", "r", stdin);
+    setbuf(stdout, NULL);
+    freopen("/home/drac/Documents/CP/output.txt", "w", stdout);
+    while (scan2(n, k) && n && k) {
+    	solve();
+    }
+}
