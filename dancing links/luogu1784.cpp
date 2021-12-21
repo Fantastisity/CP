315389ac34e5856a63e6dc2490bc7a05e6a40674
Jack <61994603+Fantastisity@users.noreply.github.com>
2021-12-21 17:11:44 +1100

    Update luogu1784.cpp

--- dancing links/luogu1784.cpp
+++ dancing links/luogu1784.cpp
@@ -115,8 +115,8 @@ void solve() {
 }
 
 int main() {
-	freopen("/home/drac/Documents/CP/input.txt", "r", stdin);
+    freopen("/home/drac/Documents/CP/input.txt", "r", stdin);
     setbuf(stdout, NULL);
     freopen("/home/drac/Documents/CP/output.txt", "w", stdout);
-	solve();
+    solve();
 }
